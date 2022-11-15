505e626e1754e9b7e59b4e3c3869b5772d4a6cf2
Kirill <kir.suprun@gmail.com>
2022-11-15 21:18:57 +0200

    take grid nest

--- iss_location_sk/src/App.js
+++ iss_location_sk/src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       
-      <h1> Privet </h1>
+      <div className='video'> 
 
+          <div className="full_screen">
+            <div className="full_screen_body"> 
+              <main className='container app_container'> 
 
+                <div className='iss_location bg-danger'>  Location of ISS    </div>
 
+                <div className='iss_location bg-success'> Data and time now     </div>
 
+                <div className='iss_location bg-warning'> Point on map, where ISS     </div>
+
+                <div className='iss_location bg-info'>  Members on ISS    </div>
+
+              </main>
+            </div>
+         
+            <video loop autoPlay muted preload="auto" className="full_screen_video" width="">
+                <source type="video/mp4" src="./assets/StarsVideo.mp4" />
+            </video>
+        </div>
+
+      </div>
 
     </div>
   );
